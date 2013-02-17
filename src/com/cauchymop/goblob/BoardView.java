d13e1cf9e1beaf808ee5d481da59402c0f48d1cd
abela <Jerome.Abela@gmail.com>
2013-02-17 19:17:14 +0000

    Fix indent.

--- src/com/cauchymop/goblob/BoardView.java
+++ src/com/cauchymop/goblob/BoardView.java
@@ -29,106 +29,106 @@ public class BoardView extends View {
   private int cellSizeInPixels;
 
   private Map<BoardContent.ContentColor, Paint> colorToPaint = ImmutableMap.of(
-	  ContentColor.White, createPaint(Color.RED),
-	  ContentColor.Black, createPaint(Color.GREEN),
-	  ContentColor.WhiteTerritory, createPaint(Color.MAGENTA),
-	  ContentColor.BlackTerritory, createPaint(Color.CYAN),
-	  ContentColor.Empty, createPaint(Color.GRAY)
-	  );
+      ContentColor.White, createPaint(Color.RED),
+      ContentColor.Black, createPaint(Color.GREEN),
+      ContentColor.WhiteTerritory, createPaint(Color.MAGENTA),
+      ContentColor.BlackTerritory, createPaint(Color.CYAN),
+      ContentColor.Empty, createPaint(Color.GRAY)
+  );
 
   /**
    * @param color
    * @return
    */
   public Paint createPaint(int color) {
-	Paint p = new Paint();
-	p.setColor(color);
-	return p;
+    Paint p = new Paint();
+    p.setColor(color);
+    return p;
   }
 
   public BoardView(Context context, AttributeSet attrs) {
-	super(context, attrs);
+    super(context, attrs);
   }
 
   public BoardView(Context context, AttributeSet attrs, int defStyle) {
-	super(context, attrs, defStyle);
+    super(context, attrs, defStyle);
   }
 
   @Override
   protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-	super.onSizeChanged(w, h, oldw, oldh);
-	boardSizeInPixels = Math.min(getWidth(), getHeight());
-	marginX = (getWidth() - boardSizeInPixels) / 2;
-	marginY = (getHeight() - boardSizeInPixels) / 2;
-	cellSizeInPixels = boardSizeInPixels / boardSizeInCells;
+    super.onSizeChanged(w, h, oldw, oldh);
+    boardSizeInPixels = Math.min(getWidth(), getHeight());
+    marginX = (getWidth() - boardSizeInPixels) / 2;
+    marginY = (getHeight() - boardSizeInPixels) / 2;
+    cellSizeInPixels = boardSizeInPixels / boardSizeInCells;
   }
 
   @Override
   public boolean onTouchEvent(MotionEvent event) {
-	final int action = event.getAction();
-	int nb_fingers = event.getPointerCount();
+    final int action = event.getAction();
+    int nb_fingers = event.getPointerCount();
 
-	if (nb_fingers != 1) {
-	  return false;
-	}
+    if (nb_fingers != 1) {
+      return false;
+    }
 
-	int x = (int) ((event.getX() - marginX) / cellSizeInPixels);
-	int y = (int) ((event.getY() - marginY) / cellSizeInPixels);
-	if (y < 0 || y >= boardSizeInCells || x < 0 || x >= boardSizeInCells) {
-	  lastClickedCellCoord = null;
-	  return false;
-	}
-	switch (action & MotionEvent.ACTION_MASK) {
-	  case MotionEvent.ACTION_DOWN: {
-		// Log.i("TOUCH EVENT", "ACTION_DOWN: row:" + row +" col:" + col);
-		lastClickedCellCoord = new Point(x, y);
-		return true;
-	  }
+    int x = (int) ((event.getX() - marginX) / cellSizeInPixels);
+    int y = (int) ((event.getY() - marginY) / cellSizeInPixels);
+    if (y < 0 || y >= boardSizeInCells || x < 0 || x >= boardSizeInCells) {
+      lastClickedCellCoord = null;
+      return false;
+    }
+    switch (action & MotionEvent.ACTION_MASK) {
+      case MotionEvent.ACTION_DOWN: {
+        // Log.i("TOUCH EVENT", "ACTION_DOWN: row:" + row +" col:" + col);
+        lastClickedCellCoord = new Point(x, y);
+        return true;
+      }
 
-	  case MotionEvent.ACTION_UP: {
-		// Log.i("TOUCH EVENT", "ACTION_UP: row:" + row +" col:" + col);
-		if (lastClickedCellCoord != null && lastClickedCellCoord.x == x
-		    && lastClickedCellCoord.y == y) {
-		  board.setContentColor(x, y, currentPlayerColor);
-		  lastClickedCellCoord = null;
-		  endTurn();
-		  invalidate();
-		  return true;
-		}
-	  }
-	}
+      case MotionEvent.ACTION_UP: {
+        // Log.i("TOUCH EVENT", "ACTION_UP: row:" + row +" col:" + col);
+        if (lastClickedCellCoord != null && lastClickedCellCoord.x == x
+            && lastClickedCellCoord.y == y) {
+          board.setContentColor(x, y, currentPlayerColor);
+          lastClickedCellCoord = null;
+          endTurn();
+          invalidate();
+          return true;
+        }
+      }
+    }
 
-	return false;
+    return false;
   }
 
   private void endTurn() {
-	if (currentPlayerColor == ContentColor.Black) {
-	  currentPlayerColor = ContentColor.White;
-	} else {
-	  currentPlayerColor = ContentColor.Black;
-	}
+    if (currentPlayerColor == ContentColor.Black) {
+      currentPlayerColor = ContentColor.White;
+    } else {
+      currentPlayerColor = ContentColor.Black;
+    }
   }
 
   @Override
   protected void onDraw(Canvas canvas) {
-	super.onDraw(canvas);
-	int canvasBoardSizeInPixels = Math.min(canvas.getWidth(), canvas.getHeight());
-	int canvasMarginX = (canvas.getWidth() - canvasBoardSizeInPixels) / 2;
-	int canvasMarginY = (canvas.getHeight() - canvasBoardSizeInPixels) / 2;
-	int canvasCellSizeInPixels = canvasBoardSizeInPixels / boardSizeInCells;
-	RectF r = new RectF();
-	for (int x = 0; x < boardSizeInCells; x++) {
-	  for (int y = 0; y < boardSizeInCells; y++) {
-		r.set(canvasMarginX + (canvasCellSizeInPixels * x),
-		    canvasMarginY + (canvasCellSizeInPixels * y),
-		    canvasMarginX + (canvasCellSizeInPixels * (x + 1)),
-		    canvasMarginY + (canvasCellSizeInPixels * (y + 1)));
-		ContentColor contentColor = board.getContentColor(x, y);
-		// Log.i("", x + "," + y + ": " + contentColor);
-		Paint paint = colorToPaint.get(contentColor);
-		canvas.drawRect(r, paint);
-	  }
-	}
+    super.onDraw(canvas);
+    int canvasBoardSizeInPixels = Math.min(canvas.getWidth(), canvas.getHeight());
+    int canvasMarginX = (canvas.getWidth() - canvasBoardSizeInPixels) / 2;
+    int canvasMarginY = (canvas.getHeight() - canvasBoardSizeInPixels) / 2;
+    int canvasCellSizeInPixels = canvasBoardSizeInPixels / boardSizeInCells;
+    RectF r = new RectF();
+    for (int x = 0; x < boardSizeInCells; x++) {
+      for (int y = 0; y < boardSizeInCells; y++) {
+        r.set(canvasMarginX + (canvasCellSizeInPixels * x),
+            canvasMarginY + (canvasCellSizeInPixels * y),
+            canvasMarginX + (canvasCellSizeInPixels * (x + 1)),
+            canvasMarginY + (canvasCellSizeInPixels * (y + 1)));
+        ContentColor contentColor = board.getContentColor(x, y);
+        // Log.i("", x + "," + y + ": " + contentColor);
+        Paint paint = colorToPaint.get(contentColor);
+        canvas.drawRect(r, paint);
+      }
+    }
   }
 
 }
--- src/com/cauchymop/goblob/GoBoard.java
+++ src/com/cauchymop/goblob/GoBoard.java
@@ -7,131 +7,131 @@ import java.util.HashSet;
  */
 public class GoBoard {
 
-    private final int boardSizeInCells;
-    private Color board[];
+  private final int boardSizeInCells;
+  private Color board[];
   private int boardSize;
 
   public GoBoard(int boardSizeInCells) {
-        this.boardSizeInCells = boardSizeInCells;
-        initEmptyCells();
-        initBorderCells();
-    }
+    this.boardSizeInCells = boardSizeInCells;
+    initEmptyCells();
+    initBorderCells();
+  }
 
-    /**
-     * Plays a move.
-     *
-     * @param color the {@link Color} being played
-     * @param x the x coordinate for the move (0 to {@code boardSizeInCells}-1)
-     * @param y the y coordinate for the move (0 to {@code boardSizeInCells}-1)
-     * @return whether the move was valid and played
-     */
-    public boolean play(Color color, int x, int y) {
-        int pos = getPos(x, y);
-        if (board[pos] != Color.Empty) {
-            return false;
-        }
-        board[pos] = color;
-        captureNeighbors(pos);
-        if (getLiberties(pos).isEmpty()) {
-            board[pos] = Color.Empty;
-            return false;
-        }
-        return true;
+  /**
+   * Plays a move.
+   *
+   * @param color the {@link Color} being played
+   * @param x the x coordinate for the move (0 to {@code boardSizeInCells}-1)
+   * @param y the y coordinate for the move (0 to {@code boardSizeInCells}-1)
+   * @return whether the move was valid and played
+   */
+  public boolean play(Color color, int x, int y) {
+    int pos = getPos(x, y);
+    if (board[pos] != Color.Empty) {
+      return false;
     }
+    board[pos] = color;
+    captureNeighbors(pos);
+    if (getLiberties(pos).isEmpty()) {
+      board[pos] = Color.Empty;
+      return false;
+    }
+    return true;
+  }
 
-    private Color getOpponent(Color color) {
-        if (color == Color.Black) {
-            return Color.White;
-        }
-        if (color == Color.White) {
-            return Color.Black;
-        }
-        throw new RuntimeException("Invalid color " + color);
+  private Color getOpponent(Color color) {
+    if (color == Color.Black) {
+      return Color.White;
     }
+    if (color == Color.White) {
+      return Color.Black;
+    }
+    throw new RuntimeException("Invalid color " + color);
+  }
 
-    private void captureNeighbors(int pos) {
-        Color opponent = getOpponent(board[pos]);
-        HashSet<Integer> captured = new HashSet<Integer>();
-        findCapturedNeighbors(getNorth(pos), opponent, captured);
-        findCapturedNeighbors(getSouth(pos), opponent, captured);
-        findCapturedNeighbors(getEast(pos), opponent, captured);
-        findCapturedNeighbors(getWest(pos), opponent, captured);
-        for (Integer capturedPosition : captured) {
-            board[capturedPosition] = Color.Empty;
-        }
+  private void captureNeighbors(int pos) {
+    Color opponent = getOpponent(board[pos]);
+    HashSet<Integer> captured = new HashSet<Integer>();
+    findCapturedNeighbors(getNorth(pos), opponent, captured);
+    findCapturedNeighbors(getSouth(pos), opponent, captured);
+    findCapturedNeighbors(getEast(pos), opponent, captured);
+    findCapturedNeighbors(getWest(pos), opponent, captured);
+    for (Integer capturedPosition : captured) {
+      board[capturedPosition] = Color.Empty;
     }
+  }
 
-    private void findCapturedNeighbors(int pos, Color opponent, HashSet<Integer> captured) {
-        if (board[pos] != opponent) {
-            return;
-        }
-        HashSet<Integer> liberties = new HashSet<Integer>();
-        HashSet<Integer> stones = new HashSet<Integer>();
-        getGroupInfo(board[pos], pos, stones, liberties);
-        if (liberties.isEmpty()) {
-            captured.addAll(stones);
-        }
+  private void findCapturedNeighbors(int pos, Color opponent, HashSet<Integer> captured) {
+    if (board[pos] != opponent) {
+      return;
     }
+    HashSet<Integer> liberties = new HashSet<Integer>();
+    HashSet<Integer> stones = new HashSet<Integer>();
+    getGroupInfo(board[pos], pos, stones, liberties);
+    if (liberties.isEmpty()) {
+      captured.addAll(stones);
+    }
+  }
 
-    private HashSet<Integer> getLiberties(int pos) {
-        HashSet<Integer> liberties = new HashSet<Integer>();
-        HashSet<Integer> stones = new HashSet<Integer>();
-        getGroupInfo(board[pos], pos, stones, liberties);
-        return liberties;
-    }
+  private HashSet<Integer> getLiberties(int pos) {
+    HashSet<Integer> liberties = new HashSet<Integer>();
+    HashSet<Integer> stones = new HashSet<Integer>();
+    getGroupInfo(board[pos], pos, stones, liberties);
+    return liberties;
+  }
 
-    private void getGroupInfo(Color color, int pos, HashSet<Integer> stones, HashSet<Integer> liberties) {
-        if (stones.contains(pos) || liberties.contains(pos)) {
-            return;
-        }
-        if (board[pos] == color) {
-            stones.add(pos);
-            getGroupInfo(color, getNorth(pos), stones, liberties);
-            getGroupInfo(color, getSouth(pos), stones, liberties);
-            getGroupInfo(color, getWest(pos), stones, liberties);
-            getGroupInfo(color, getEast(pos), stones, liberties);
-        } else if (board[pos] == Color.Empty) {
-            liberties.add(pos);
-        }
+  private void getGroupInfo(Color color, int pos, HashSet<Integer> stones, HashSet<Integer> liberties) {
+    if (stones.contains(pos) || liberties.contains(pos)) {
+      return;
     }
+    if (board[pos] == color) {
+      stones.add(pos);
+      getGroupInfo(color, getNorth(pos), stones, liberties);
+      getGroupInfo(color, getSouth(pos), stones, liberties);
+      getGroupInfo(color, getWest(pos), stones, liberties);
+      getGroupInfo(color, getEast(pos), stones, liberties);
+    } else if (board[pos] == Color.Empty) {
+      liberties.add(pos);
+    }
+  }
 
-    private int getNorth(int pos) {
-        return pos - (boardSizeInCells + 2);
-    }
+  private int getNorth(int pos) {
+    return pos - (boardSizeInCells + 2);
+  }
 
-    private int getSouth(int pos) {
-        return pos + (boardSizeInCells + 2);
-    }
+  private int getSouth(int pos) {
+    return pos + (boardSizeInCells + 2);
+  }
 
-    private int getWest(int pos) {
-        return pos - 1;
-    }
+  private int getWest(int pos) {
+    return pos - 1;
+  }
 
-    private int getEast(int pos) {
-        return pos + 1;
-    }
+  private int getEast(int pos) {
+    return pos + 1;
+  }
 
-    private void initEmptyCells() {
-        board = new Color[(boardSizeInCells+2)*(boardSizeInCells+2)];
-        for (int x = 0 ; x < boardSizeInCells+2 ; x++) {
-            for (int y = 0 ; y < boardSizeInCells ; y++) {
-                board[getPos(x, y)] = Color.Empty;
-            }
-        }
+  private void initEmptyCells() {
+    board = new Color[(boardSizeInCells+2)*(boardSizeInCells+2)];
+    for (int x = 0 ; x < boardSizeInCells+2 ; x++) {
+      for (int y = 0 ; y < boardSizeInCells ; y++) {
+        board[getPos(x, y)] = Color.Empty;
+      }
     }
+  }
 
-    private void initBorderCells() {
-        for (int col = 0 ; col < boardSizeInCells ; col++) {
-            board[getPos(0, col)] = Color.Border;
-            board[getPos(col, 0)] = Color.Border;
-            board[getPos(boardSizeInCells-1, col)] = Color.Border;
-            board[getPos(col, boardSizeInCells-1)] = Color.Border;
-        }
+  private void initBorderCells() {
+    for (int col = 0 ; col < boardSizeInCells ; col++) {
+      board[getPos(0, col)] = Color.Border;
+      board[getPos(col, 0)] = Color.Border;
+      board[getPos(boardSizeInCells-1, col)] = Color.Border;
+      board[getPos(col, boardSizeInCells-1)] = Color.Border;
     }
+  }
 
-    private int getPos(int x, int y) {
-        return (y + 1) * (boardSizeInCells + 2) + (x + 1);
-    }
+  private int getPos(int x, int y) {
+    return (y + 1) * (boardSizeInCells + 2) + (x + 1);
+  }
 
   public int getBoardSize() {
     return boardSize;
@@ -142,9 +142,9 @@ public class GoBoard {
   }
 
   public static enum Color {
-        Empty,
-        Border,
-        Black,
-        White,
-    }
+    Empty,
+    Border,
+    Black,
+    White,
+  }
 }
