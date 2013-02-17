012da5bb0a0c609021af5c3a09df9121a1bed709
abela <Jerome.Abela@gmail.com>
2013-02-17 19:54:05 +0000

    Adds test project.

---
+++ tests/src/com/cauchymop/goblob/GoBoardTest.java
@@ -0,0 +1,13 @@
+package com.cauchymop.goblob;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Class to test {@link GoBoard}..
+ */
+public class GoBoardTest extends AndroidTestCase {
+
+  public void testPlay() {
+    assertTrue(false);
+  }
+}
