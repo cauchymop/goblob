1bb06eb910a0a2011756ec79bfa9bffc0913fec5
abela <Jerome.Abela@gmail.com>
2013-08-19 20:21:35 +0100

    Remove heuristics, which were neither implemented or used.

--- src/com/cauchymop/goblob/AlphaBeta.java
+++ src/com/cauchymop/goblob/AlphaBeta.java
@@ -8,14 +8,14 @@ public class AlphaBeta implements AI {
   private static final double MAX = 100.0;
   private static final int HEURISTICS_THRESHOLD = 4;
 
-  public int getBestMove(Game originalGame, Heuristic heuristic, int depth) {
+  public int getBestMove(Game originalGame, int depth) {
     Game localGame = originalGame.copy();
     int posCount = localGame.getPosCount();
     double bestResult = -MAX;
     int bestMove = -1;
     for (int move = 0 ; move < posCount; move++) {
       if (localGame.play(move)) {
-        double result = -getAlphaBeta(localGame, heuristic, depth, -MAX, MAX);
+        double result = -getAlphaBeta(localGame, depth, -MAX, MAX);
         if (result > bestResult) {
           bestResult = result;
           bestMove = move;
@@ -26,7 +26,7 @@ public class AlphaBeta implements AI {
     return bestMove;
   }
 
-  public static double getAlphaBeta(Game game, Heuristic heuristic, int depth, double min,
+  public static double getAlphaBeta(Game game, int depth, double min,
       double max) {
     double score = game.getScore();
     if (depth == 0 || game.isGameEnd() || Math.abs(score) >= 8) {
@@ -35,14 +35,8 @@ public class AlphaBeta implements AI {
     double bestScore = min;
 
     for (int pos = 0 ; pos < game.getPosCount() ; pos++) {
-      // TODO: store pos and its heuristics
-    }
-    // TODO: sort positions by heuristics
-
-    // loop over ordered positions
-    for (int pos = 0 ; pos < game.getPosCount() ; pos++) {
       if (game.play(pos)) {
-        score = -getAlphaBeta(game, heuristic, depth - 1, -max, -bestScore);
+        score = -getAlphaBeta(game, depth - 1, -max, -bestScore);
         bestScore = Math.max(score, bestScore);
         game.undo();
         if (bestScore >= max) {
@@ -52,22 +46,15 @@ public class AlphaBeta implements AI {
     }
     double reference = 0;
 
-    // store results
-    for (int pos = 0 ; pos < game.getPosCount() ; pos++) {
-      if (depth > HEURISTICS_THRESHOLD) {
-        heuristic.record(game, depth, reference, pos, score);
-      }
-    }
-
     return bestScore;
   }
 
-  public static double[] getMoveValues(Game game, Heuristic heuristic, int depth) {
+  public static double[] getMoveValues(Game game, int depth) {
     int posCount = game.getPosCount();
     double[] result = new double[posCount];
     for (int pos = 0 ; pos < posCount; pos++) {
       if (game.play(pos)) {
-        result[pos] = -getAlphaBeta(game, heuristic, depth, -MAX, MAX);
+        result[pos] = -getAlphaBeta(game, depth, -MAX, MAX);
         game.undo();
       } else {
         result[pos] = Double.NaN;
