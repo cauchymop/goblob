87fb05e1d040d764d274a11b95be426f287bb43a
Olivier Bonal <olivier.bonal@gmail.com>
2013-08-26 14:07:20 +0100

    SignIn button is now visible on first run + updated version in BaseGameUtils gradle.

--- goblob/src/main/com/cauchymop/goblob/MainActivity.java
+++ goblob/src/main/com/cauchymop/goblob/MainActivity.java
@@ -3,11 +3,14 @@ package com.cauchymop.goblob;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.example.games.basegameutils.BaseGameActivity;
 
 public class MainActivity extends BaseGameActivity {
+
+  private static final int REQUEST_ACHIEVEMENTS = 1;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,19 @@ public class MainActivity extends BaseGameActivity {
     // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.game_menu, menu);
     return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+
+    if (id == R.id.menu_achievements) {
+      startActivityForResult(getGamesClient().getAchievementsIntent(), REQUEST_ACHIEVEMENTS);
+      return true;
+    } else if (id == R.id.menu_signout) {
+      signOut(null);
+    }
+    return false;
   }
 
   public void startChallenges(View v) {
