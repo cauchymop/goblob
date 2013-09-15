1c71d98865007bb4720694a8354093b2ab08a1ca
abela <Jerome.Abela@gmail.com>
2013-09-15 17:19:57 +0100

    Clean up MainActivity by upgrading anonymous listener.

--- goblob/src/main/com/cauchymop/goblob/ui/MainActivity.java
+++ goblob/src/main/com/cauchymop/goblob/ui/MainActivity.java
@@ -23,7 +23,6 @@ import com.google.android.gms.games.multiplayer.realtime.RoomUpdateListener;
 import com.google.example.games.basegameutils.BaseGameActivity;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends BaseGameActivity {
 
@@ -34,6 +33,8 @@ public class MainActivity extends BaseGameActivity {
   private Room gameRoom;
   private GameFragment gameFragment;
   private Participant opponent;
+
+  private RoomStatusUpdateListener gameRoomStatusListener = new BaseRoomStatusUpdateListener();
 
   private RoomUpdateListener gameRoomUpdateListener = new RoomUpdateListener() {
 
@@ -65,58 +66,6 @@ public class MainActivity extends BaseGameActivity {
     Intent i = getGamesClient().getRealTimeWaitingRoomIntent(room, 1);
     startActivityForResult(i, WAITING_ROOM);
   }
-
-  private RoomStatusUpdateListener gameRoomStatusListener = new RoomStatusUpdateListener() {
-    @Override
-    public void onRoomConnecting(Room room) {
-      Log.d(TAG, "onRoomConnecting(" + room + ")");
-    }
-
-    @Override
-    public void onRoomAutoMatching(Room room) {
-      Log.d(TAG, "onRoomAutoMatching(" + room + ")");
-    }
-
-    @Override
-    public void onPeerInvitedToRoom(Room room, List<String> strings) {
-      Log.d(TAG, "onPeerInvitedToRoom(" + room + ", " + strings + ")");
-    }
-
-    @Override
-    public void onPeerDeclined(Room room, List<String> strings) {
-      Log.d(TAG, "onPeerDeclined(" + room + ", " + strings + ")");
-    }
-
-    @Override
-    public void onPeerJoined(Room room, List<String> strings) {
-      Log.d(TAG, "onPeerJoined(" + room + ", " + strings + ")");
-    }
-
-    @Override
-    public void onPeerLeft(Room room, List<String> strings) {
-      Log.d(TAG, "onPeerLeft(" + room + ", " + strings + ")");
-    }
-
-    @Override
-    public void onConnectedToRoom(Room room) {
-      Log.d(TAG, "onConnectedToRoom(" + room + ")");
-    }
-
-    @Override
-    public void onDisconnectedFromRoom(Room room) {
-      Log.d(TAG, "onDisconnectedFromRoom(" + room + ")");
-    }
-
-    @Override
-    public void onPeersConnected(Room room, List<String> strings) {
-      Log.d(TAG, "onPeersConnected(" + room + ", " + strings + ")");
-    }
-
-    @Override
-    public void onPeersDisconnected(Room room, List<String> strings) {
-      Log.d(TAG, "onPeersDisconnected(" + room + ", " + strings + ")");
-    }
-  };
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -371,4 +320,5 @@ public class MainActivity extends BaseGameActivity {
     getGamesClient().sendReliableRealTimeMessage(null, message, gameRoom.getRoomId(),
         opponent.getParticipantId());
   }
+
 }
