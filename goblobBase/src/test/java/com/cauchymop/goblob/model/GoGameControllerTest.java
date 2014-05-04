727eaaf1cc9e8459d1a3ee37e19aea01a5c21aea
JeromeA <Jerome.Abela@gmail.com>
2014-05-04 15:33:51 +0100

    Adds a GameConfiguration move in GameData.

--- goblobBase/src/test/java/com/cauchymop/goblob/model/GoGameControllerTest.java
+++ goblobBase/src/test/java/com/cauchymop/goblob/model/GoGameControllerTest.java
@@ -14,14 +14,19 @@ import static org.mockito.Mockito.verifyZeroInteractions;
  */
 public class GoGameControllerTest {
 
+  private static final int TEST_HANDICAP = 0;
+  private static final String TEST_BLACK_ID = "black";
+  private static final String TEST_WHITE_ID = "white";
+
   @Test
   public void testNew_gameData() {
     GameData gameData = GameData.newBuilder()
+        .addMove(GameDatas.createStartGameMove(9, TEST_HANDICAP, TEST_BLACK_ID, TEST_WHITE_ID))
         .addMove(GameDatas.createMove(2, 3))
         .addMove(GameDatas.createMove(4, 5))
         .build();
 
-    GoGameController controller = new GoGameController(gameData, 9);
+    GoGameController controller = new GoGameController(gameData);
 
     assertThat(controller.getGameData()).isEqualTo(gameData);
     GoGame goGame = controller.getGame();
@@ -31,7 +36,7 @@ public class GoGameControllerTest {
 
   @Test
   public void testGetGameData() {
-    GoGameController controller = new GoGameController(GameData.getDefaultInstance(), 9);
+    GoGameController controller = createGoGameController();
     DummyPlayerController whiteController = new DummyPlayerController();
     controller.setWhiteController(whiteController);
     DummyPlayerController blackController = new DummyPlayerController();
@@ -46,15 +51,19 @@ public class GoGameControllerTest {
         .build());
   }
 
+  private GoGameController createGoGameController() {
+    return new GoGameController(GameDatas.createGameData(9, TEST_HANDICAP, TEST_BLACK_ID, TEST_WHITE_ID));
+  }
+
   @Test
   public void testToString() {
-    GoGameController controller = new GoGameController(GameData.getDefaultInstance(), 9);
+    GoGameController controller = createGoGameController();
     assertThat(controller.toString()).isNotNull();
   }
 
   @Test
   public void testFireGameChanged() {
-    GoGameController controller = new GoGameController(GameData.getDefaultInstance(), 9);
+    GoGameController controller = createGoGameController();
     GoGameController.Listener mockListener1 = mock(GoGameController.Listener.class);
     GoGameController.Listener mockListener2 = mock(GoGameController.Listener.class);
     controller.addListener(mockListener1);
@@ -67,7 +76,7 @@ public class GoGameControllerTest {
 
   @Test
   public void testRemoveListener() {
-    GoGameController controller = new GoGameController(GameData.getDefaultInstance(), 9);
+    GoGameController controller = createGoGameController();
     GoGameController.Listener mockListener = mock(GoGameController.Listener.class);
     controller.addListener(mockListener);
     controller.removeListener(mockListener);
