9b32d8d742d2dd269e49f1a8c295838e8e401eb2
JeromeA <Jerome.Abela@gmail.com>
2015-01-06 22:58:32 +0000

    Better match menu items

--- goblob/src/main/java/com/cauchymop/goblob/ui/MatchesAdapter.java
+++ goblob/src/main/java/com/cauchymop/goblob/ui/MatchesAdapter.java
@@ -2,6 +2,7 @@ package com.cauchymop.goblob.ui;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 import com.cauchymop.goblob.R;
 
 import java.util.List;
+import java.util.zip.Inflater;
 
 /**
  * Adapter displaying MatchMenuItem from the actionbar navigation spinner.
@@ -18,16 +20,27 @@ import java.util.List;
 public class MatchesAdapter extends ArrayAdapter<MatchMenuItem> {
 
   public MatchesAdapter(Context context, List<MatchMenuItem> items) {
-    super(context, R.layout.match_row_view, R.id.label, items);
+    super(context, R.layout.match_row_view, items);
   }
 
   @Override @NonNull
   public View getDropDownView(int position, View convertView, ViewGroup parent) {
-    final View matchRowView = super.getDropDownView(position, convertView, parent);
+    final View matchRowView;
+
+    if (convertView == null) {
+      LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+      matchRowView = inflater.inflate(R.layout.match_row_view, parent, false);
+    } else {
+      matchRowView = convertView;
+    }
+
     MatchMenuItem item = getItem(position);
 
-    TextView labelView = (TextView) matchRowView.findViewById( R.id.label);
-    labelView.setText(item.getDisplayName(getContext()));
+    TextView firstLineLabelView = (TextView) matchRowView.findViewById( R.id.label_first_line);
+    firstLineLabelView.setText(item.getFirstLine(getContext()));
+
+    TextView secondLineLabelView = (TextView) matchRowView.findViewById( R.id.label_second_line);
+    secondLineLabelView.setText(item.getSecondLine(getContext()));
 
     ImageView iconView = (ImageView) matchRowView.findViewById( R.id.match_type_icon);
     iconView.setImageDrawable(item.getIcon(getContext()));
