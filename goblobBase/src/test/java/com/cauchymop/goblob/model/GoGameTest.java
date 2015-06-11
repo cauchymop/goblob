e8df970a72ff2d32b782e9894c59f8605b00a13a
JeromeA <Jerome.Abela@gmail.com>
2015-06-11 21:30:53 +0100

    Support for handicap/komi in local games

--- goblobBase/src/test/java/com/cauchymop/goblob/model/GoGameTest.java
+++ goblobBase/src/test/java/com/cauchymop/goblob/model/GoGameTest.java
@@ -15,7 +15,7 @@ public class GoGameTest {
 
   @Test
   public void test_Serializable() {
-    GoGame goGame = new GoGame(9);
+    GoGame goGame = new GoGame(9, 0);
     goGame.play(7);
     GoGame goGameAfterSerialization = SerializableTester.reserialize(goGame);
 
@@ -24,7 +24,7 @@ public class GoGameTest {
 
   @Test
   public void test_Serializable_empty() {
-    GoGame goGame = new GoGame(9);
+    GoGame goGame = new GoGame(9, 0);
     GoGame goGameAfterSerialization = SerializableTester.reserialize(goGame);
 
     assertThat(goGameAfterSerialization).isEqualTo(goGame);
@@ -32,13 +32,13 @@ public class GoGameTest {
 
   @Test
   public void testGetMoveHistory_noMoves() {
-    GoGame goGame = new GoGame(9);
+    GoGame goGame = new GoGame(9, 0);
     assertThat(goGame.getMoveHistory()).isEmpty();
   }
 
   @Test
   public void testGetMoveHistory() {
-    GoGame goGame = new GoGame(9);
+    GoGame goGame = new GoGame(9, 0);
     goGame.play(11);
     goGame.play(17);
     assertThat(goGame.getMoveHistory()).containsExactly(11, 17);
@@ -46,7 +46,7 @@ public class GoGameTest {
 
   @Test
   public void testIsGameEnd() {
-    GoGame goGame = new GoGame(9);
+    GoGame goGame = new GoGame(9, 0);
     assertThat(goGame.isGameEnd()).isFalse();
 
     goGame.play(2);
@@ -64,36 +64,36 @@ public class GoGameTest {
 
   @Test
   public void testUndo() {
-    GoGame goGame = new GoGame(9);
+    GoGame goGame = new GoGame(9, 0);
     goGame.play(2);
     goGame.undo();
 
-    assertThat(goGame).isEqualTo(new GoGame(9));
+    assertThat(goGame).isEqualTo(new GoGame(9, 0));
   }
 
   @Test
   public void testEquals() {
-    GoGame goGame9A = new GoGame(9);
-    GoGame goGame9B = new GoGame(9);
+    GoGame goGame9A = new GoGame(9, 0);
+    GoGame goGame9B = new GoGame(9, 0);
     GoGame goGame9C = goGame9A.copy();
 
-    GoGame goGame13A = new GoGame(13);
-    GoGame goGame13B = new GoGame(13);
+    GoGame goGame13A = new GoGame(13, 0);
+    GoGame goGame13B = new GoGame(13, 0);
     GoGame goGame13C = goGame13A.copy();
 
-    GoGame goGame19A = new GoGame(19);
-    GoGame goGame19B = new GoGame(19);
+    GoGame goGame19A = new GoGame(19, 0);
+    GoGame goGame19B = new GoGame(19, 0);
     GoGame goGame19C = goGame19A.copy();
 
-    GoGame goGameMoveA = new GoGame(19);
+    GoGame goGameMoveA = new GoGame(19, 0);
     goGameMoveA.play(7);
-    GoGame goGameMoveB = new GoGame(19);
+    GoGame goGameMoveB = new GoGame(19, 0);
     goGameMoveB.play(7);
     GoGame goGameMoveC = goGameMoveA.copy();
 
-    GoGame goGameOtherMoveA = new GoGame(19);
+    GoGame goGameOtherMoveA = new GoGame(19, 0);
     goGameOtherMoveA.play(11);
-    GoGame goGameOtherMoveB = new GoGame(19);
+    GoGame goGameOtherMoveB = new GoGame(19, 0);
     goGameOtherMoveB.play(11);
     GoGame goGameOtherMoveC = goGameOtherMoveA.copy();
 
@@ -108,7 +108,7 @@ public class GoGameTest {
 
   @Test
   public void testPlay_move() {
-    GoGame goGame = new GoGame(9);
+    GoGame goGame = new GoGame(9, 0);
     assertThat(goGame.play(3)).isTrue();
     assertThat(goGame.getMoveHistory()).containsExactly(3);
     assertThat(goGame.getColor(3, 0)).isEqualTo(Color.BLACK);
@@ -118,7 +118,7 @@ public class GoGameTest {
 
   @Test
   public void testPlay_ko() {
-    GoGame goGame = new GoGame(9);
+    GoGame goGame = new GoGame(9, 0);
     goGame.play(3);
     assertThat(goGame.getMoveHistory()).containsExactly(3);
     assertThat(goGame.getColor(3, 0)).isEqualTo(Color.BLACK);
@@ -128,7 +128,7 @@ public class GoGameTest {
 
   @Test
   public void testPlay_pass() {
-    GoGame goGame = new GoGame(9);
+    GoGame goGame = new GoGame(9, 0);
     assertThat(goGame.play(goGame.getPassValue())).isTrue();
     assertThat(goGame.getMoveHistory()).containsExactly(goGame.getPassValue());
     assertThat(goGame.getCurrentColor()).isEqualTo(Color.WHITE);
@@ -137,7 +137,7 @@ public class GoGameTest {
 
   @Test
   public void testPlay_invalid() {
-    GoGame goGame = new GoGame(9);
+    GoGame goGame = new GoGame(9, 0);
     goGame.play(3);
     assertThat(goGame.play(3)).isFalse();
   }
