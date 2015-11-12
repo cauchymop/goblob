181ec58bb65c154165a12051780a1d6c02efd7b8
abela <jerome.abela@gmail.com>
2015-11-12 20:25:07 +0000

    Fixed GogameControllerTest and introduced Dagger Test Module

--- goblobBase/src/test/java/com/cauchymop/goblob/model/GoGameControllerTest.java
+++ goblobBase/src/test/java/com/cauchymop/goblob/model/GoGameControllerTest.java
@@ -1,9 +1,12 @@
 package com.cauchymop.goblob.model;
 
+import com.cauchymop.goblob.injection.Injector;
 import com.cauchymop.goblob.proto.PlayGameData;
 import com.cauchymop.goblob.proto.PlayGameData.GoPlayer;
 
 import org.junit.Test;
+
+import dagger.ObjectGraph;
 
 import static com.cauchymop.goblob.proto.PlayGameData.GameData;
 import static org.fest.assertions.Assertions.assertThat;
@@ -15,15 +18,22 @@ public class GoGameControllerTest {
 
   private static final int TEST_HANDICAP = 0;
   private static final float TEST_KOMI = 7.5f;
-  private static final GoPlayer TEST_BLACK_PLAYER = GameDatas.createGamePlayer("blackid", "black");
-  private static final GoPlayer TEST_WHITE_PLAYER = GameDatas.createGamePlayer("whiteid", "white");
+  private static final GameDatas GAME_DATAS = new GameDatas();
+  private static final GoPlayer TEST_BLACK_PLAYER = GAME_DATAS.createGamePlayer("blackid", "black");
+  private static final GoPlayer TEST_WHITE_PLAYER = GAME_DATAS.createGamePlayer("whiteid", "white");
+
+  static {
+    ObjectGraph objectGraph = ObjectGraph.create(new GoApplicationTestModule());
+    Injector.setObjectGraph(objectGraph);
+  }
 
   @Test
   public void testNew_gameData() {
     GameData gameData = GameData.newBuilder()
-        .setGameConfiguration(GameDatas.createGameConfiguration(9, TEST_HANDICAP, TEST_KOMI, PlayGameData.GameType.LOCAL, TEST_BLACK_PLAYER, TEST_WHITE_PLAYER))
-        .addMove(GameDatas.createMove(2, 3))
-        .addMove(GameDatas.createMove(4, 5))
+        .setGameConfiguration(GAME_DATAS.createGameConfiguration(9, TEST_HANDICAP, TEST_KOMI, PlayGameData.GameType.LOCAL, TEST_BLACK_PLAYER, TEST_WHITE_PLAYER))
+        .addMove(GAME_DATAS.createMove(2, 3))
+        .addMove(GAME_DATAS.createMove(4, 5))
+        .setVersion(GAME_DATAS.VERSION)
         .build();
 
     GoGameController controller = new GoGameController(gameData, null);
@@ -36,23 +46,24 @@ public class GoGameControllerTest {
 
   @Test
   public void testGetGameData() {
-    PlayGameData.GameConfiguration gameConfiguration = GameDatas.createGameConfiguration(9, TEST_HANDICAP, TEST_KOMI, PlayGameData.GameType.LOCAL, TEST_BLACK_PLAYER, TEST_WHITE_PLAYER);
+    PlayGameData.GameConfiguration gameConfiguration = GAME_DATAS.createGameConfiguration(9, TEST_HANDICAP, TEST_KOMI, PlayGameData.GameType.LOCAL, TEST_BLACK_PLAYER, TEST_WHITE_PLAYER);
     GoGameController controller = new GoGameController(GameData.newBuilder()
         .setGameConfiguration(gameConfiguration)
         .build(), null);
-    controller.playMove(GameDatas.createMove(0, 0));
-    controller.playMove(GameDatas.createMove(1, 1));
-    controller.playMove(GameDatas.createPassMove());
+    controller.playMove(GAME_DATAS.createMove(0, 0));
+    controller.playMove(GAME_DATAS.createMove(1, 1));
+    controller.playMove(GAME_DATAS.createPassMove());
     assertThat(controller.getGameData()).isEqualTo(GameData.newBuilder()
         .setGameConfiguration(gameConfiguration)
-        .addMove(GameDatas.createMove(0, 0))
-        .addMove(GameDatas.createMove(1, 1))
-        .addMove(GameDatas.createPassMove())
+        .addMove(GAME_DATAS.createMove(0, 0))
+        .addMove(GAME_DATAS.createMove(1, 1))
+        .addMove(GAME_DATAS.createPassMove())
+        .setVersion(GAME_DATAS.VERSION)
         .build());
   }
 
   private GoGameController createGoGameController() {
-    return new GoGameController(GameDatas.createGameData(9, TEST_HANDICAP, 0, PlayGameData.GameType.LOCAL, TEST_BLACK_PLAYER, TEST_WHITE_PLAYER), null);
+    return new GoGameController(GAME_DATAS.createGameData(9, TEST_HANDICAP, 0, PlayGameData.GameType.LOCAL, TEST_BLACK_PLAYER, TEST_WHITE_PLAYER), null);
   }
 
   @Test
