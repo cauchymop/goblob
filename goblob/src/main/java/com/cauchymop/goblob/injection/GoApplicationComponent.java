e65637bc1f457a7bc81fec83a623c07c3132c4cb
abela <jerome.abela@gmail.com>
2016-01-19 21:18:06 +0000

    Migrate to Dagger 2

---
+++ goblob/src/main/java/com/cauchymop/goblob/injection/GoApplicationComponent.java
@@ -0,0 +1,22 @@
+package com.cauchymop.goblob.injection;
+
+import com.cauchymop.goblob.ui.GameConfigurationFragment;
+import com.cauchymop.goblob.ui.GameFragment;
+import com.cauchymop.goblob.ui.MainActivity;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Dagger Component to instantiate injected objects.
+ */
+@Singleton
+@Component (
+    modules= GoApplicationModule.class
+)
+public interface GoApplicationComponent {
+  void inject(MainActivity mainActivity);
+  void inject(GameConfigurationFragment gameConfigurationFragment);
+  void inject(GameFragment gameFragment);
+}
--- goblob/src/main/java/com/cauchymop/goblob/ui/GoApplication.java
+++ goblob/src/main/java/com/cauchymop/goblob/ui/GoApplication.java
@@ -1,24 +1,28 @@
 package com.cauchymop.goblob.ui;
 
 import android.app.Application;
-import android.os.Bundle;
 
-import com.cauchymop.goblob.injection.Injector;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-
-import dagger.ObjectGraph;
+import com.cauchymop.goblob.injection.DaggerGoApplicationComponent;
+import com.cauchymop.goblob.injection.GoApplicationComponent;
+import com.cauchymop.goblob.injection.GoApplicationModule;
 
 /**
  * Top level application for Game of Go.
  */
 public class GoApplication extends Application {
 
+  private GoApplicationComponent component;
+
   @Override
   public void onCreate() {
     super.onCreate();
-    ObjectGraph objectGraph = ObjectGraph.create(new GoApplicationModule(this));
-    Injector.setObjectGraph(objectGraph);
+    component = DaggerGoApplicationComponent.builder()
+        .goApplicationModule(new GoApplicationModule(this))
+        .build();
+  }
+
+  public GoApplicationComponent getComponent() {
+    return component;
   }
 
 }
