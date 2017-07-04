c9b59069ab17d95677fd65cde5741ed7058f287e
abela <jerome.abela@gmail.com>
2017-07-04 22:52:17 +0100

    More Kotlin!

--- goblobBase/src/main/java/com/cauchymop/goblob/presenter/ConfigurationViewModelCreator.kt
+++ goblobBase/src/main/java/com/cauchymop/goblob/presenter/ConfigurationViewModelCreator.kt
@@ -7,19 +7,18 @@ import javax.inject.Inject
 
 class ConfigurationViewModelCreator @Inject constructor(val gameMessageGenerator: GameMessageGenerator) {
 
-    fun getConfigurationViewModel(goGameController: GoGameController): ConfigurationViewModel {
-        val gameConfiguration = goGameController.getGameConfiguration()
-        return ConfigurationViewModel(
-                komi = gameConfiguration.komi,
-                boardSize = gameConfiguration.boardSize,
-                handicap = gameConfiguration.handicap,
-                blackPlayerName = gameConfiguration.black.name,
-                whitePlayerName = gameConfiguration.white.name,
+    fun getConfigurationViewModel(goGameController: GoGameController) = with(goGameController.gameConfiguration) {
+        ConfigurationViewModel(
+                komi = komi,
+                boardSize = boardSize,
+                handicap = handicap,
+                blackPlayerName = black.name,
+                whitePlayerName = white.name,
                 message = getMessage(goGameController),
                 interactionsEnabled = goGameController.isLocalTurn())
     }
 
-    private fun getMessage(goGameController: GoGameController): String = when {
+    private fun getMessage(goGameController: GoGameController) = when {
         goGameController.phase == INITIAL -> gameMessageGenerator.configurationMessageInitial
         goGameController.isLocalTurn -> gameMessageGenerator.configurationMessageAcceptOrChange
         else -> gameMessageGenerator.configurationMessageWaitingForOpponent
