ac392e5154a4e39eae2ec8ccdf9dc4af10f9a272
JeromeA <jerome.abela@gmail.com>
2018-05-03 23:10:21 +0100

    More Tests

--- goblobBase/src/test/java/com/cauchymop/goblob/viewmodel/InGameViewModelsTest.kt
+++ goblobBase/src/test/java/com/cauchymop/goblob/viewmodel/InGameViewModelsTest.kt
@@ -2,14 +2,19 @@ package com.cauchymop.goblob.viewmodel
 
 import com.cauchymop.goblob.model.*
 import com.cauchymop.goblob.presenter.GameMessageGenerator
+import com.cauchymop.goblob.proto.PlayGameData
 import com.cauchymop.goblob.proto.PlayGameData.GameData.Phase.*
+import com.google.common.truth.Truth.*
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Answers
+import org.mockito.BDDMockito
+import org.mockito.BDDMockito.*
 import org.mockito.Mock
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.spy
 import org.mockito.junit.MockitoJUnitRunner
 import kotlin.test.assertFailsWith
 
@@ -39,5 +44,22 @@ class InGameViewModelsTest {
     goGameController.gameData = createGameData().setPhase(CONFIGURATION).build()
 
     assertFailsWith<IllegalArgumentException> { inGameViewModels.from(goGameController) }
+  }
+
+  @Test
+  fun from_currentPlayer() {
+
+  }
+
+  @Test
+  fun from_currentPlayerViewModel() {
+    goGameController.gameData = createGameData().build()
+    val mockContoller = spy(goGameController)
+    given(mockContoller.currentPlayer).willReturn(PlayGameData.GoPlayer.newBuilder().setName("🍕").setId("id").build())
+    given(mockContoller.currentColor).willReturn(PlayGameData.Color.WHITE)
+
+    val actual = inGameViewModels.from(mockContoller)
+
+    assertThat(actual.currentPlayerViewModel).isEqualTo(PlayerViewModel("🍕", PlayGameData.Color.WHITE))
   }
 }
