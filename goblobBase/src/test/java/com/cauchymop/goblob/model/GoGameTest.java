0ec6bf863dfa02012b8c5ccdffbcaaf36721e783
JeromeA <jerome.abela@gmail.com>
2018-07-05 21:48:08 +0100

    A Bit more coverage

--- goblobBase/src/test/java/com/cauchymop/goblob/model/GoGameTest.java
+++ goblobBase/src/test/java/com/cauchymop/goblob/model/GoGameTest.java
@@ -5,6 +5,8 @@ import com.google.common.testing.SerializableTester;
 
 import org.junit.Test;
 
+import kotlin.Pair;
+
 import static com.cauchymop.goblob.proto.PlayGameData.Color;
 import static com.google.common.truth.Truth.assertThat;
 
@@ -119,11 +121,12 @@ public class GoGameTest {
   @Test
   public void testPlay_ko() {
     GoGame goGame = new GoGame(9, 0);
-    goGame.play(3);
-    assertThat(goGame.getMoveHistory()).containsExactly(3);
-    assertThat(goGame.getColor(3, 0)).isEqualTo(Color.BLACK);
-    assertThat(goGame.getCurrentColor()).isEqualTo(Color.WHITE);
-    assertThat(goGame.isLastMovePass()).isFalse();
+    assertThat(goGame.play(goGame.getPos(2, 0))).isTrue();
+    assertThat(goGame.play(goGame.getPos(1, 0))).isTrue();
+    assertThat(goGame.play(goGame.getPos(1, 1))).isTrue();
+    assertThat(goGame.play(goGame.getPos(0, 1))).isTrue();
+    assertThat(goGame.play(goGame.getPos(0, 0))).isTrue();
+    assertThat(goGame.play(goGame.getPos(1, 0))).isFalse();
   }
 
   @Test
@@ -143,18 +146,186 @@ public class GoGameTest {
   }
 
   @Test
-  public void testHandicap9() {
+  public void testHandicap9x9_9() {
     GoGame goGame = new GoGame(9, 9);
     assertThat(TextBoard.INSTANCE.toString(goGame.getBoard())).isEqualTo(
         ".........\n" +
-        ".........\n" +
-        "..●.●.●..\n" +
-        ".........\n" +
-        "..●.●.●..\n" +
-        ".........\n" +
-        "..●.●.●..\n" +
-        ".........\n" +
-        ".........\n"
+            ".........\n" +
+            "..●.●.●..\n" +
+            ".........\n" +
+            "..●.●.●..\n" +
+            ".........\n" +
+            "..●.●.●..\n" +
+            ".........\n" +
+            ".........\n"
     );
+  }
+
+  @Test
+  public void testHandicap9x9_2() {
+    GoGame goGame = new GoGame(9, 2);
+    assertThat(TextBoard.INSTANCE.toString(goGame.getBoard())).isAnyOf(
+        ".........\n" +
+            ".........\n" +
+            "..●......\n" +
+            ".........\n" +
+            ".........\n" +
+            ".........\n" +
+            "......●..\n" +
+            ".........\n" +
+            ".........\n",
+        ".........\n" +
+            ".........\n" +
+            "......●..\n" +
+            ".........\n" +
+            ".........\n" +
+            ".........\n" +
+            "..●......\n" +
+            ".........\n" +
+            ".........\n"
+
+    );
+  }
+
+  @Test
+  public void testHandicap9x9_3() {
+    GoGame goGame = new GoGame(9, 3);
+    assertThat(TextBoard.INSTANCE.toString(goGame.getBoard())).isAnyOf(
+        ".........\n" +
+            ".........\n" +
+            "..●......\n" +
+            ".........\n" +
+            ".........\n" +
+            ".........\n" +
+            "..●...●..\n" +
+            ".........\n" +
+            ".........\n",
+        ".........\n" +
+            ".........\n" +
+            "..●...●..\n" +
+            ".........\n" +
+            ".........\n" +
+            ".........\n" +
+            "..●......\n" +
+            ".........\n" +
+            ".........\n",
+        ".........\n" +
+            ".........\n" +
+            "..●...●..\n" +
+            ".........\n" +
+            ".........\n" +
+            ".........\n" +
+            "......●..\n" +
+            ".........\n" +
+            ".........\n",
+        ".........\n" +
+            ".........\n" +
+            "......●..\n" +
+            ".........\n" +
+            ".........\n" +
+            ".........\n" +
+            "..●...●..\n" +
+            ".........\n" +
+            ".........\n"
+
+    );
+  }
+
+  @Test
+  public void testHandicap9x9_4() {
+    GoGame goGame = new GoGame(9, 4);
+    assertThat(TextBoard.INSTANCE.toString(goGame.getBoard())).isEqualTo(
+        ".........\n" +
+            ".........\n" +
+            "..●...●..\n" +
+            ".........\n" +
+            ".........\n" +
+            ".........\n" +
+            "..●...●..\n" +
+            ".........\n" +
+            ".........\n"
+    );
+  }
+
+  @Test
+  public void testHandicap9x9_5() {
+    GoGame goGame = new GoGame(9, 5);
+    assertThat(TextBoard.INSTANCE.toString(goGame.getBoard())).isEqualTo(
+        ".........\n" +
+            ".........\n" +
+            "..●...●..\n" +
+            ".........\n" +
+            "....●....\n" +
+            ".........\n" +
+            "..●...●..\n" +
+            ".........\n" +
+            ".........\n"
+    );
+  }
+
+  @Test
+  public void testHandicap9x9_7() {
+    GoGame goGame = new GoGame(9, 7);
+    assertThat(TextBoard.INSTANCE.toString(goGame.getBoard())).isAnyOf(
+        ".........\n" +
+            ".........\n" +
+            "..●...●..\n" +
+            ".........\n" +
+            "..●.●.●..\n" +
+            ".........\n" +
+            "..●...●..\n" +
+            ".........\n" +
+            ".........\n",
+        ".........\n" +
+            ".........\n" +
+            "..●.●.●..\n" +
+            ".........\n" +
+            "....●....\n" +
+            ".........\n" +
+            "..●.●.●..\n" +
+            ".........\n" +
+            ".........\n"
+
+    );
+  }
+
+  @Test
+  public void testHandicap19x19_9() {
+    GoGame goGame = new GoGame(19, 9);
+    assertThat(TextBoard.INSTANCE.toString(goGame.getBoard())).isEqualTo(
+        "...................\n" +
+            "...................\n" +
+            "...................\n" +
+            "...●.....●.....●...\n" +
+            "...................\n" +
+            "...................\n" +
+            "...................\n" +
+            "...................\n" +
+            "...................\n" +
+            "...●.....●.....●...\n" +
+            "...................\n" +
+            "...................\n" +
+            "...................\n" +
+            "...................\n" +
+            "...................\n" +
+            "...●.....●.....●...\n" +
+            "...................\n" +
+            "...................\n" +
+            "...................\n"
+    );
+  }
+
+  @Test
+  public void testGetPos() {
+    GoGame goGame = new GoGame(9, 9);
+    assertThat(goGame.getPos(5, 0)).isEqualTo(5);
+    assertThat(goGame.getPos(0, 5)).isEqualTo(5 * 9);
+  }
+
+  @Test
+  public void testGetLastMoveXY() {
+    GoGame goGame = new GoGame(9, 9);
+    goGame.play(goGame.getPos(2, 3));
+    assertThat(goGame.getLastMoveXY()).isEqualTo(new Pair<>(2, 3));
   }
 }
