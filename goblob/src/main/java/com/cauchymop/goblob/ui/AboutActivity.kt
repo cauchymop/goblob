a02207b94794af5a160c2dec1e980784ec26e4fd
JeromeA <jerome.abela@gmail.com>
2018-09-18 22:42:08 +0100

    Better About Screen

--- goblob/src/main/java/com/cauchymop/goblob/ui/AboutActivity.kt
+++ goblob/src/main/java/com/cauchymop/goblob/ui/AboutActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.Html
 import android.text.method.LinkMovementMethod
+import android.view.MenuItem
 import com.cauchymop.goblob.R
 import kotlinx.android.synthetic.main.activity_about.*
 
@@ -12,7 +13,20 @@ class AboutActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_about)
+    setSupportActionBar(app_toolbar)
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    setTitle(R.string.about)
     about_text.text = Html.fromHtml(getString(R.string.about_text))
     about_text.setMovementMethod(LinkMovementMethod.getInstance())
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    when (item?.itemId) {
+      android.R.id.home -> {
+        finish()
+        return true
+      }
+    }
+    return super.onOptionsItemSelected(item)
   }
 }
